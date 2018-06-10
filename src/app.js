20035d8a329aba620814f3f42c2002a4184d964a
brunoguazina <bruno@guazina.com>
2018-06-10 18:02:20 -0300

    Adding Access Control

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 const app = require('./config/express')();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 const rotas = require('./app/routes/index')(app);
 
-app.listen(3000, function () {
+app.listen(3001, function () {
     console.log('Servidor rodando...');
 })
