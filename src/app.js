d5ae627159be718336dbacd9bd76ba225d779071
brunoguazina <bruno@guazina.com>
2019-04-04 09:52:52 -0400

    adjustment app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const app = require('./config/express')();
+const app = require('express')();
 const rotas = require('./app/routes/index')(app);
 
 var port = process.env.PORT || 1337;
 app.listen(port);
 
-console.log("Server running at", port);
+console.log("Server running at http://localhost:" + port);
