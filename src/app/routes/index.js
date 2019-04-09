69ec21889acb9f9f79a2b6efefb252531a4c70fe
brunoguazina <bruno@guazina.com>
2019-04-09 10:51:53 -0400

    update

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,14 +1,14 @@
 module.exports = function (app) {
     
     const projetos = require('./projects');
-    const projetos = require('./project');
+    const projeto = require('./project');
     const brands = require('./brands');
     const about = require('./about');
     
     app.use('/about', about.get);
     app.use('/brands', brands.get);
     app.use('/projects', projetos.get);
-    app.use('/project/:id', projetos.getById);
+    app.use('/project/:id', projeto.getById);
 }
 
 
