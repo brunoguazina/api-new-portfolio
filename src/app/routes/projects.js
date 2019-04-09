21d096624f2e87798777b0ccc4a5fa3b00ce20a3
brunoguazina <bruno@guazina.com>
2019-04-08 21:12:13 -0400

    reversing adjustments

--- src/app/routes/projects.js
+++ src/app/routes/projects.js
@@ -1,14 +1,13 @@
-let data = require('../data/projects_db');
-let value = req.params.id;
-
 exports.get = (req, res, next) => {
-    
+    let data = require('../data/projects_db');
     console.log(`passou aqui! - Projetos`);
     res.status(200).json(data.projects);
 };
 
 exports.getById = (req, res, next) => {
     
+    let value = req.params.id;
+    let data = require('../data/projects_db');
     let projects = data.projects;
     
     function isProject(project) { 
