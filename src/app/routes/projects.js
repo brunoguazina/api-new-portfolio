ebe0526169a1e5613a317c35d6891a9c788b8d99
brunoguazina <bruno@guazina.com>
2019-04-09 10:02:44 -0400

    adjustments

--- src/app/routes/projects.js
+++ src/app/routes/projects.js
@@ -1,19 +1,22 @@
 let data = require('../data/projects_db');
-let value = req.params.id;
+
 
 exports.get = (req, res, next) => {
     
     console.log(`passou aqui! - Projetos`);
+    
     res.status(200).json(data.projects);
 };
 
 exports.getById = (req, res, next) => {
     
     let projects = data.projects;
-    
+    let value = req.params.id;
+
     function isProject(project) { 
         return project.url === value;
     }
     console.log(`passou aqui! - ${value}`);
+
     res.status(200).json(projects.find(isProject));
 };
