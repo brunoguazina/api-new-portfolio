071ac4e54a9bc4c0b77bbe0b03b46cf57f979c50
brunoguazina <bruno@guazina.com>
2019-04-09 10:42:43 -0400

    adjustments in the router and variable name

--- src/app/routes/projects.js
+++ src/app/routes/projects.js
@@ -1,6 +1,8 @@
 
 exports.get = (req, res, next) => {
+
     let data = require('../data/projects_db');
+
     console.log(`passou aqui! - Projetos`);
     
     res.status(200).json(data.projects);
@@ -10,12 +12,13 @@ exports.getById = (req, res, next) => {
     
     let data = require('../data/projects_db');
     let projects = data.projects;
-    let value = req.params.id;
+    let project = req.params.id;
 
     function isProject(project) { 
-        return project.url === value;
+        return project.url === project;
     }
-    console.log(`passou aqui! - ${value}`);
+
+    console.log(`passou aqui! - ${project}`);
 
     res.status(200).json(projects.find(isProject));
 };
